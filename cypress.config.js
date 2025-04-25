3f1b8afaefe68ce664763715d0a181d1456a748e
Yesh <indanayeswanth@gmail.com>
2025-04-25 19:38:26 +0530

    faker and emailintegration

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,47 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+  
+//     reporter: 'mochawesome',
+//     reporterOptions: {
+//       reportDir: 'cypress/reports/mocha',  // base report dir
+//       overwrite: false,
+//       html: true,
+//       json: true,
+      
+//     },
+  
+//   video: true,
+//   // videosFolder: 'cypress/videos',
+
+//   e2e: {
+//     reporter: 'cypress-mochawesome-reporter',
+//     setupNodeEvents(on, config) {
+//       require('cypress-mochawesome-reporter/plugin')(on);
+//     },
+   
+//   }
+// });
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports/mocha',  // base report dir
-      overwrite: false,
-      html: true,
-      json: true,
-      timestamp: 'mmddyyyy_HHMMss'
-    },
-  
-  video: true,
-  // videosFolder: 'cypress/videos',
-
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html',
+    overwrite: true,
+    html: true,
+    json: false,
+    timestamp: 'mmddyyyy_HHMMss'
+  },
   e2e: {
-    reporter: 'cypress-mochawesome-reporter',
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-    },
-    env: {
-      baseurl: "https://naveenautomationlabs.com/opencart/index.php",
-      loginurl: "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
-      registerurl: "https://naveenautomationlabs.com/opencart/index.php?route=account/register"
-    }
+    setupNodeEvents(on, config) {}
+  },
+  env: {
+    baseurl: "https://naveenautomationlabs.com/opencart/index.php",
+    loginurl: "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
+    registerurl: "https://naveenautomationlabs.com/opencart/index.php?route=account/register"
   }
+
 });
+
